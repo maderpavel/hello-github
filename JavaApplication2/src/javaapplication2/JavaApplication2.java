b6fb72847be868b2f5c5161bf843dd69f7024f8e
pmader <pmader@otenergy.eu>
2016-01-20 09:40:54 +0100

    Commit to master

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,9 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        /* prvni zmena v master */
     }
     
 }
